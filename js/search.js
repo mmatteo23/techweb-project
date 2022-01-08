ee09e1cf24707c0903d521181af6aba78141e858
Davide-Milan <davidemilan27@gmail.com>
2022-01-08 13:27:27 +0100

    Fix search bar javascript - ora non muove niente

--- js/search.js
+++ js/search.js
@@ -1,13 +1,28 @@
 const src = document.getElementById("search");
 const src_bar_wrapper = document.getElementById("src-bar-wrapper");
 const src_bar = document.getElementById("src-bar");
+const breadcrumb = document.getElementById("breadcrumb");
 
 src.addEventListener('click', function(){
     if(src_bar_wrapper.style.display != 'block'){
         src_bar_wrapper.style.display = 'block';
+        if(window.innerWidth >= 570)
+            breadcrumb.style.marginTop = '-40px';
     } else{
         src_bar_wrapper.style.display = 'none';
+        if(window.innerWidth >= 570)
+            breadcrumb.style.marginTop = '0';
     }
     src_bar.focus();
 }
 );
+window.onresize = function() {
+    console.log(window.innerWidth + " " + src_bar_wrapper.style.display);
+    if(window.innerWidth >=570 && src_bar_wrapper.style.display == 'block'){
+        breadcrumb.style.marginTop = '-40px';
+    }
+    else if(window.innerWidth >=570 && src_bar_wrapper.style.display != 'block')
+        breadcrumb.style.marginTop = '0';
+    else if(window.innerWidth <570)
+        breadcrumb.style.marginTop = '0';
+}
