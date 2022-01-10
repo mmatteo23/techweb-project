53c4c5f3385a8f9af101e7db848d791e24252152
Danilo Stojkovic <danilostojkovic749@gmail.com>
2022-01-10 17:16:40 +0100

    games tag risolto

--- games.php
+++ games.php
@@ -15,30 +15,26 @@ $user_output = "";  // html code to send to the page
 if($conn_ok){
     $games = $connection_manager->getGames();
     $game_tags = $connection_manager->getGamesTags();
+
     $connection_manager->closeDBConnection();
     if($games != null){  // there's at least one game exists        
         $num_of_games = count($games);
+        $x=0;
         for($i = 0; $i < $num_of_games; $i++){
             $game = $games[$i];            
             $user_output .= '<li class="card" id="'.$game['name'].'">
             <a href="search.php?game='.urlencode($game['name']).'"><img src="/images/games/' . $game['game_img'] . '" alt="' . $game['name'] . ' cover" class="card-img"></a>
             <div class="card-content">
-                <h2 class="card-title"><a href="search.php?game='.urlencode($game['name']).'" class="card-title-link">' . $game['name'] . '</a></h2>
-                    <ul class="tag-list">';
-                $x = 0;
-                $game_tag = $game_tags[$x];
-                $found = false;
-                while(($found == false) or ($game_tag['game_id'] == $game['id'])){
-                    if($game_tag['game_id'] == $game['id']) {
-                        $found = true;
-                        $user_output .= '<li class="tag"><a href="search.php?tag='.urlencode($game_tag['name']).'">'. $game_tag['name'] .'</a></li>';
+                <h2 class="card-title"><a href="search.php?game='.urlencode($game['name']).'" class="card-title-link">' . $game['name'] . '</a></h2>';
+                if($game_tags[$x]['game_id']==$game['id']){
+                    $user_output .= '<ul class="tag-list">';
+                    while(($game_tags[$x]['game_id'] == $game['id'])){
+                        $user_output .= '<li class="tag"><a href="search.php?tag='.urlencode($game_tags[$x]['name']).'">'. $game_tags[$x]['name'] .'</a></li>';
+                        $x++;                     
                     }
-                    $x++;
-                    $game_tag = $game_tags[$x];                        
-                }
-                $user_output .= '</ul>
-                            </div>
-                        </li>';
+                    $user_output .= '</ul>';
+                }   
+                $user_output .= '</div></li>';
         }
     }
 } else { 
