a645214fb0479763941d4a1173e8e6adf36eec36
Davide-Milan <davidemilan27@gmail.com>
2022-01-13 00:26:07 +0100

    Inizio print.css + reso bottone search accessibile
    ABBIAMO QUASI FINITO DAI!

--- js/search.js
+++ js/search.js
@@ -3,26 +3,28 @@ const src_bar_wrapper = document.getElementById("src-bar-wrapper");
 const src_bar = document.getElementById("src-bar");
 const breadcrumb = document.getElementById("breadcrumb");
 
-src.addEventListener('click', function(){
+// src.addEventListener('click', function()
+
+function toggleSearchBar(){
     if(src_bar_wrapper.style.display != 'block'){
         src_bar_wrapper.style.display = 'block';
-        if(window.innerWidth >= 570)
+        if(window.innerWidth >= 769)
             breadcrumb.style.marginTop = '-40px';
     } else{
         src_bar_wrapper.style.display = 'none';
-        if(window.innerWidth >= 570)
+        if(window.innerWidth >= 769)
             breadcrumb.style.marginTop = '0';
     }
     src_bar.focus();
     src.blur(); 
 }
-);
+
 window.onresize = function() {
-    if(window.innerWidth >=570 && src_bar_wrapper.style.display == 'block'){
+    if(window.innerWidth >=769 && src_bar_wrapper.style.display == 'block'){
         breadcrumb.style.marginTop = '-40px';
     }
-    else if(window.innerWidth >=570 && src_bar_wrapper.style.display != 'block')
+    else if(window.innerWidth >=769 && src_bar_wrapper.style.display != 'block')
         breadcrumb.style.marginTop = '0';
-    else if(window.innerWidth <570)
+    else if(window.innerWidth <769)
         breadcrumb.style.marginTop = '0';
 }
