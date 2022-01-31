fdb1bbf8e8f92e73351e64ce94bf2660b72cccd6
Davide-Milan <davidemilan27@gmail.com>
2022-01-31 19:25:54 +0100

    cose per immagini db

--- db/popolamento_db_completo.sql
+++ db/popolamento_db_completo.sql
@@ -18,14 +18,14 @@ VALUES(
     'League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.',
     '2009:10:27',
     'Riot Games',
-    'league_of_legends.jpg'
+    '1.jpg'
 ),
 (
     'Valorant',
     'Blend your style and experience on a global, competitive stage. You have 13 rounds to attack and defend your side using sharp gunplay and tactical abilities. And, with one life per-round, you’ll need to think faster than your opponent if you want to survive. Take on foes across Competitive and Unranked modes as well as Deathmatch and Spike Rush.',
     '2020:06:02',
     'Riot Games',
-    'valorant.jpg'
+    '2.jpg'
 ),
 (
     'Clash Royale',
@@ -33,7 +33,7 @@ VALUES(
     Collect and upgrade dozens of cards featuring the Clash of Clans troops, spells and defenses you know and love, as well as the Royales: Princes, Knights, Baby Dragons and more. Knock the enemy King and Princesses from their towers to defeat your opponents and win Trophies, Crowns and glory in the Arena. Form a Clan to share cards and build your very own battle community.',
     '2016:03:02',
     'Supercell',
-    'clash_royale.jpg'
+    '3.jpg'
 ),
 (
     'Apex Legends',
@@ -43,35 +43,35 @@ VALUES(
     Apex Legends takes place in an immersive universe where the story continues to evolve, maps change each season, and new Legends keep joining the fight. Make your mark on the Apex Games with a multitude of distinct outfits and join the adventure!',
     '2019:02:04',
     'Respawn Entertainment',
-    'apex_legends.jpg'
+    '4.jpg'
 ),
 (
     'Rocket League',
     'Rocket League, the sequel to Supersonic Acrobatic Rocket-Powered Battle-Cars, is a popular videogame that combines soccer and cars into its gameplay. For those who enjoy high-octane reckless driving and the world’s most popular sport, Rocket League is the game for you. This cross-genre arena battler is the latest craze on PC, PS4, Xbox One and Nintendo Switch. Fun for all players and abilities, the game offers both a casual and competitive atmosphere, as well as a ton of laughs.',
     '2015:07:07',
     'Psyonix Studios',
-    'rocket_league.jpg'
+    '5.jpg'
 ),
 (
     'Counter-Strike: Global Offensive',
     'Counter-Strike: Global Offensive (CS: GO) expands upon the team-based action gameplay that it pioneered when it was launched 19 years ago. CS: GO features new maps, characters, weapons, and game modes, and delivers updated versions of the classic CS content ',
     '2012:08:21',
     'Valve Corporation',
-    'cs_go.jpg'
+    '6.jpg'
 ),
 (
     'DOTA 2',
     'Every day, millions of players worldwide enter the battle as one of over a hundred Dota Heroes in a 5v5 team clash. Dota is the deepest multi-player action RTS game ever made and there’s always a new strategy or tactic to discover. It’s completely free to play and always will be - start defending your ancient now.',
     '2013:07:09',
     'Valve Corporation',
-    'dota_2.jpg'
+    '7.jpg'
 ),
 (
     'FIFA',
     'The legendary FIFA series has been produced by EA SPORTS for over 20 years, and is now the largest sports video game franchise on the planet. FIFA brings The World’s Game to life, letting you play with the biggest leagues, clubs, and players in world football, all with incredible detail and realism. Whether you want to build your dream squad in FIFA Ultimate Team, lead your favourite club to glory in Career Mode, take the game back to the streets with EA SPORTS VOLTA FOOTBALL, or get bragging rights over a friend in Kick-Off Mode, the FIFA series lets you play The World’s Game your way.',
     '1993:12:15',
     'Electronic Arts',
-    'fifa.jpg'
+    '8.jpg'
 ),
 (
     'Fortnite',
@@ -79,35 +79,35 @@ VALUES(
     Fortnite Battle Royale is the free 100-player PvP mode in Fortnite. One giant map, a battle bus, Fortnite building skills and destructible environments combined with intense PvP combat while the last one standing wins; and Save the World is a cooperative shooter-survival game with up to 4-man squad to fight off husks and defend mission objectives with fortifications and defenses.',
     '2017:07:21',
     'Epic Games',
-    'fortnite.jpg'
+    '9.jpg'
 ),
 (
     'Minecraft',
     'Minecraft is a sandbox construction video game developed by Mojang Studios where players interact with a fully modifiable three-dimensional environment made of blocks and entities. Its diverse gameplay lets players choose the way they play, allowing for countless possibilities.',
     '2011:11:18',
     'Mojang',
-    'minecraft.jpg'
+    '10.jpg'
 ),
 (
     'Overwatch',
     'Overwatch is a vibrant team-based shooter set on a near-future earth. Every match is an intense 6v6 battle between a cast of unique heroes, each with their own incredible powers and abilities. Clash in over 20 maps from across the globe, and switch heroes on the fly to adapt to the ever-changing situation on the field.',
     '2016:05:24',
     'Blizzard Entertainment',
-    'overwatch.jpg'
+    '11.jpg'
 ),
 (
     'Warzone',
     'Warzone™ Pacific arrives with a new map, trading in the Eastern European front of Verdansk for the sunny Pacific island of Caldera. With plenty of tropical landscapes to explore, there’s always some action above and below the surface. And for an action-packed World War II experience, take to the skies in "Vanguard Royale”, a new Warzone-based game mode featuring a fighter plane and Anti-Aircraft vehicles.',
     '2020:03:10',
     'Activision',
-    'warzone.jpg'
+    '12.jpg'
 ),
 (
     'Pok&#233;mon Unite',
     'Pok&#233;mon Unite is a free-to-play videogame published by Timi Studio Group for Nintendo Swtich Console first.',
     '2021:07:21',
     'TiMi Studio Group',
-    'pokemon_unite.jpg'
+    '13.jpg'
 );
 
 INSERT INTO Article (id, title, subtitle, text, publication_date, cover_img, alt_cover_img, read_time, author, game_id) VALUES 
